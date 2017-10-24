8d7acef5a850551f68c895e7a76e4748ee4db303
Shikha <shikhafadnavis@gmail.com>
2017-10-23 23:16:49 -0700

    Sq and mul begin

---
+++ rsa-keygen.go
@@ -0,0 +1,27 @@
+/*
+
+	Author: Shikha Fadnavis
+	Program : RSA key generation
+	Date: 10/24/2017
+
+*/
+
+package main
+
+import(
+	"fmt"
+	"crypto/rand"
+	"strings"
+)
+
+
+func squareAndMultiply(num int, exp int) int{
+
+	strconv.FormatInt(,2)
+
+
+
+}
+
+
+
