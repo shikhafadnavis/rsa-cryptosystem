4cd2b25db0e4f16d5445e9573da717561091ae1f
Shikha <shikhafadnavis@gmail.com>
2017-10-28 11:36:38 -0700

    Changed Datatype to bigInt. Run-time error in miller-rabin

--- rsa-keygen.go
+++ rsa-keygen.go
@@ -12,35 +12,40 @@ import(
 	"fmt"
 //	"crypto/rand"
 //	"strings"
-	"strconv"
+//	"strconv"
 //	"reflect"
-	"math"
-	"math/rand"
+//	"math"
+	"math/big"
+//	"math/rand"
 )
 
 
-func squareAndMultiply(num int, exp int64) int{
+func squareAndMultiply(num *big.Int, exp *big.Int) (*big.Int){
 
-	var i, res int
-	binExp := strconv.FormatInt(exp,2)
+	var i int 
+	var res *big.Int
+	//binExp := strconv.FormatInt(exp,2)
 	//fmt.Println(binExp)
 
 	//fmt.Println(reflect.TypeOf(binExp))
 	//fmt.Println(len(binExp))
 	//Start square and multiply
-	
+	binExp := fmt.Sprintf("%b", exp)
 	res = num
-	if exp == 1{
+	if exp == big.NewInt(1){
 		return num
 	}
 	for i = 1; i < len(binExp); i++{
 		if binExp[i] == 49{
 			//sq and mul
-			res = res * res
-			res = res * num
+			//res = res * res
+			res.Mul(res,res)
+			//res = res * num
+			res.Mul(res,num)
 		}else{
 			//only sq
-			res = res * res
+			//res = res * res
+			res.Mul(res,res)
 
 		}
 	}
@@ -50,49 +55,55 @@ func squareAndMultiply(num int, exp int64) int{
 
 }
 
-func millerRabinPrime(num float64) bool{
+func millerRabinPrime(num *big.Int) bool{
 
-	var a, k, res, prevRes, factor, pow float64
-	var b, nextb int64
+	var a, k, res, prevRes, factor, pow, modulus, mulRes *big.Int //float64
+	var b, nextb, i *big.Int //int64
 	var result bool = true
-	a = num - 1
-	k = 1
+	
+	a.Sub(num,big.NewInt(1))
+	fmt.Println("Num minus 1 is: ", a)
+	k = big.NewInt(1)
 	for true{
 		prevRes = res
-		res = a/math.Exp2(k)
-		if res == float64(int64(res)){
-			k++
+		div := squareAndMultiply(big.NewInt(2), k)
+		res.Div(a,div)
+		if modulus.Mod(a,div) == big.NewInt(0){
+			k.Add(k,big.NewInt(1))
 		}else{
 			break
 		}
 	}
 
 	factor = prevRes
-	pow = k-1
+	pow.Sub(k,big.NewInt(1))
 
-	fmt.Printf("the two numbers are %f and %f", factor, pow)
+	fmt.Println("the two numbers are: ", factor, pow)
 
 	for j := 0; j < 5; j++{
 	
-		randomNum := rand.Int63n(int64(num-4)) + 2
-		fmt.Printf("\n Random Number chosen is: %d", randomNum)
-		//randomNum := 2
-		b = int64(squareAndMultiply(int(randomNum), int64(factor))) % int64(num)
+		//randomNum := rand.Int63n(int64(num-4)) + 2
+		//fmt.Printf("\n Random Number chosen is: %d", randomNum)
+		randomNum := big.NewInt(2)
+		b.Mod(squareAndMultiply(randomNum, factor), num)
+		//b = int64(squareAndMultiply(int(randomNum), int64(factor))) % int64(num)
 		//b = int64(math.Exp2(factor)) % int64(num)
 		fmt.Println("\nValue of b0 is: ", b)
-		if b == 1 || b == int64(num-1){
+		if b == big.NewInt(1) || b == big.NewInt(0).Sub(num,big.NewInt(1)){
 			result = true
 			return result
 		}
-		//fmt.Println("Composite")
-		for i := 0; i < int(k-1); i++{
-                        nextb = (b*b) % int64(num)
+
+		for i = big.NewInt(0); i.Cmp(pow) == -1; i.Add(i,big.NewInt(1)){
+                        //nextb = (b*b) % int64(num)
+			mulRes.Mul(b,b)
+			nextb.Mod(mulRes, num)
                        	fmt.Println("\nValue is: ", nextb)
-                       	if nextb == 1{
+                       	if nextb == big.NewInt(1){
 				result = false
 				return result
 			}
-			if nextb == int64(num-1){
+			if nextb == big.NewInt(0).Sub(num,big.NewInt(1)){
 				result = true
 				return result
 			}
@@ -118,7 +129,7 @@ func main(){
 	//expRes := squareAndMultiply(2,1)
 	//fmt.Println(expRes)
 	//fmt.Printf("%d raised to the power of %d is: %d",expRes,)
-	primeRes := millerRabinPrime(281)
+	primeRes := millerRabinPrime(big.NewInt(85))
 	if primeRes == true{
 		fmt.Println("\n Prime number")
 	}else{
