919304c67f52026052e6cf332eb2c096bd552eba
Shikha <shikhafadnavis@gmail.com>
2017-10-29 11:49:22 -0700

    Pseudo Random Prime Number generation works

--- rsa-keygen.go
+++ rsa-keygen.go
@@ -14,45 +14,48 @@ import(
 	"math/big"
 //	"io"
 //	"math/rand"
+//	"os"
 )
 
 
-func squareAndMultiplyWithMod(num *big.Int, exp *big.Int, modn *big.Int) (*big.Int){
+func squareAndMultiplyWithMod(a *big.Int, a2 *big.Int, b *big.Int, c *big.Int) (*big.Int){
 
 	var i int 
-	var res, finalRes, preFinalRes *big.Int
-	//binExp := strconv.FormatInt(exp,2)
-	//fmt.Println(binExp)
+	var startVal, res, someRes, preRes *big.Int
 
-	//fmt.Println(reflect.TypeOf(binExp))
-	//fmt.Println(len(binExp))
-	//Start square and multiply
-	binExp := fmt.Sprintf("%b", exp)
-	res = big.NewInt(num.Int64())
-	finalRes = big.NewInt(0)
-	preFinalRes = big.NewInt(0)
-	if exp == big.NewInt(1){
-		return num
+	binExp := fmt.Sprintf("%b", b)
+	if b == big.NewInt(1){
+		return a
 	}
-	for i = 1; i < len(binExp); i++{
+
+	// Retain original value
+	startVal = big.NewInt(0)
+	startVal.Mod(a,c)
+
+	res = big.NewInt(0)
+	res.Mod(a2,c)
+
+	//res = big.NewInt(startVal.Int64())
+	fmt.Println("Received Value: ", res)
+
+	for i = 1; i < len(binExp); i++{	
+		// Square regardless
+		someRes = big.NewInt(0)
+		someRes.Mul(res, res)
+		res.Mod(someRes, c)
+
 		if binExp[i] == 49{
-			//sq and mul
-			res.Mul(res,res)
-			preFinalRes.Mod(res,modn)
-			res.Mul(preFinalRes,num)
-			finalRes.Mod(res,modn)
-//			fmt.Println("Result so far: ", finalRes)
-			
-		}else{
-			//only sq
-			res.Mul(res,res)
-			finalRes.Mod(res,num)
-//			fmt.Println("Result so far: ", finalRes)
-
+			preRes = big.NewInt(0)
+			preRes.Mul(res, startVal)
+			res.Mod(preRes,c)			
 		}
+
 	}
-//	fmt.Println("Returning sq and mul with mod result: ", finalRes)
-	return finalRes
+
+
+	fmt.Println("Returning value: ", res)
+	return res	
+		
 
 
 }
@@ -61,14 +64,9 @@ func squareAndMultiply(num *big.Int, exp *big.Int) (*big.Int){
 
 	var i int 
 	var res *big.Int
-	//binExp := strconv.FormatInt(exp,2)
-	//fmt.Println(binExp)
-
-	//fmt.Println(reflect.TypeOf(binExp))
-	//fmt.Println(len(binExp))
 	//Start square and multiply
 	binExp := fmt.Sprintf("%b", exp)
-	res = big.NewInt(num.Int64())
+//	res = big.NewInt(num.Int64())
 	if exp == big.NewInt(1){
 		return num
 	}
@@ -96,23 +94,16 @@ func millerTest(num *big.Int, factor *big.Int, pow *big.Int) bool{
 
 	b := big.NewInt(0)
 	mulRes := big.NewInt(0)
-	nextb := big.NewInt(0)
+	//nextb := big.NewInt(0)
 	var i *big.Int
 
-
-//	randomNum1 := rand.Int63n((big.NewInt(0).Sub(num,big.NewInt(4))).Int64()) + 2
 	randNumMiller := big.NewInt(0)
 	randNumByteMiller := make([]byte,64)
         crypt.Read(randNumByteMiller)
         fmt.Println("Random number byte array is: ", randNumByteMiller)
         randNumMiller.SetBytes(randNumByteMiller)
 
-
-	//fmt.Printf("\n Random Number chosen is: %d", randomNum1)
-	//Change this to crypto/rand
-	//randomNum := big.NewInt(randomNum1)
-	//b.Mod(squareAndMultiply(randomNum, factor), num)
-	b = squareAndMultiplyWithMod(randNumMiller, factor, num)
+	b = squareAndMultiplyWithMod(randNumMiller, randNumMiller, factor, num)
 	fmt.Println("\nValue of b0 is: ", b)
 	if b.Cmp(big.NewInt(1)) == 0 || b.Cmp(big.NewInt(0).Sub(num,big.NewInt(1))) == 0{
 	
@@ -121,18 +112,18 @@ func millerTest(num *big.Int, factor *big.Int, pow *big.Int) bool{
 
 	for i = big.NewInt(0); i.Cmp(pow) == -1; i.Add(i,big.NewInt(1)){
         	mulRes.Mul(b,b)
-		nextb.Mod(mulRes, num)
-                fmt.Println("\nValue is: ", nextb)
-                if nextb.Cmp(big.NewInt(1)) == 0{
+		b.Mod(mulRes, num)
+                fmt.Println("\nValue is: ", b)
+                if b.Cmp(big.NewInt(1)) == 0{
 			
 			return false
 		}
-		if nextb.Cmp(big.NewInt(0).Sub(num,big.NewInt(1))) == 0{
+		if b.Cmp(big.NewInt(0).Sub(num,big.NewInt(1))) == 0{
 			
 			return true
 		}
 
-                b = big.NewInt(nextb.Int64())
+		fmt.Println("Subsequent b value is: ", b)
 
         }// end of squaring for
  
@@ -144,23 +135,20 @@ func millerTest(num *big.Int, factor *big.Int, pow *big.Int) bool{
 
 func millerRabinPrime(num *big.Int) bool{
 
-	var prevRes, factor *big.Int //float64
+	var factor *big.Int //float64
 	
 
 	a := big.NewInt(0)	
 	a.Sub(num,big.NewInt(1))
 	fmt.Println("Num minus 1 is: ", a)
-	k := big.NewInt(1)
-	res := big.NewInt(0)
+	k := big.NewInt(0) // Set to 1 for original
+	//res := big.NewInt(0)
 	modulus := big.NewInt(0)
 	for true{
-		prevRes = big.NewInt(res.Int64())
-		div := squareAndMultiply(big.NewInt(2), k)
-		fmt.Println("Num will be divided by: ", div)
-		res.Div(a,div)
-		fmt.Println("resulting number is: ", res)
-		fmt.Println("resulting remainder is: ", modulus.Mod(a,div))
-		modulus.Mod(a,div)
+		a.Div(a,big.NewInt(2)) // Change to ,div for original
+		fmt.Println("resulting number is: ", a)
+		fmt.Println("resulting remainder is: ", modulus.Mod(a,big.NewInt(2)))
+		modulus.Mod(a,big.NewInt(2))
 		if modulus.Cmp(big.NewInt(0)) == 0{
 			k.Add(k,big.NewInt(1))
 			fmt.Println("k is now; ", k)
@@ -169,9 +157,11 @@ func millerRabinPrime(num *big.Int) bool{
 		}
 	}
 
-	factor = big.NewInt(prevRes.Int64())
+	//factor = big.NewInt(prevRes.Int64())
+	factor = big.NewInt(0)
+	factor.Mul(a,big.NewInt(2))
 	pow := big.NewInt(0)
-	pow.Sub(k,big.NewInt(1))
+	pow.Sub(k,big.NewInt(0)) // Set to 1 for original
 
 	fmt.Println("the two numbers are: ", factor, pow)
 
@@ -189,38 +179,35 @@ func millerRabinPrime(num *big.Int) bool{
 } // end of func
 
 func main(){
-/*	var Reader io.Reader
-	randNum, err := rand.Int(Reader, big.NewInt(20000))
-	if err != nil{
-		panic(err)
-	}
-*/
+
 	//randNum := big.NewInt(224737)
 	randNum := big.NewInt(0)
-//	randNum.SetString("28342763623",10)
+//	randNum.SetString("5737894193278481003132906692801828456005280097628154082002898285238645282246643593433683475946138489071845817699635707465770454796912355070188166454999529",10)
 	for true{
-	randNumByte := make([]byte,64)
-	crypt.Read(randNumByte)
-	fmt.Println("Random number byte array is: ", randNumByte)
-	randNum.SetBytes(randNumByte)
-	fmt.Println("random number chosen is: ", randNum)
-	operation := big.NewInt(0)
-	operation.Mod(randNum, big.NewInt(2))
-	if operation.Cmp(big.NewInt(0)) == 0{
-		fmt.Println("Composite Number")
-		//generate random again
-	}else{
-		primeRes := millerRabinPrime(randNum)
-		if primeRes == true{
-			fmt.Println("\n Prime number")
-			break
+		randNumByte := make([]byte,64)
+		crypt.Read(randNumByte)
+		fmt.Println("Random number byte array is: ", randNumByte)
+		randNum.SetBytes(randNumByte)
+		fmt.Println("random number chosen is: ", randNum)
+		operation := big.NewInt(0)
+		operation.Mod(randNum, big.NewInt(2))
+		if operation.Cmp(big.NewInt(0)) == 0{
+			fmt.Println("Composite Number")
+			//generate random again
 		}else{
-			fmt.Println("\n Composite Number")
+			primeRes := millerRabinPrime(randNum)
+			if primeRes == true{
+				fmt.Println("\n Prime number")
+				break
+//				os.Exit(3)
+			}else{
+				fmt.Println("\n Composite Number")
+			}
+
 		}
 
 	}
 
-	}
 }
 
 
